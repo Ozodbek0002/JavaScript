81dae719cf3ce8f9d9a5280427ab6092f0ddcb5c
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-02-06 16:12:08 +0500

    8-dars

---
+++ 7-dars/index.js
@@ -0,0 +1,8 @@
+let myname = "Ozodbek"; 
+let first_name = "Ozodbek";
+
+
+// console.log(myname)
+
+/* fedwjsb
+fewnfkjre */
---
+++ 8-dars/index.js
@@ -0,0 +1,28 @@
+// let name = "Ozodbek";
+// var first_name = "Ozodbek";
+
+// const age = 22; // Ozgarmas
+// // age = 21; // Xatolik
+// console.log(age);
+
+
+{
+  let myname = "Ozodbek";
+  const age = 22;
+  var last_name = "Ozodov";
+}
+
+last_name = "Husanov";
+
+console.log(myname); // Xatolik
+console.log(age); // Xatolik
+console.log(last_name); // Ozodov
+
+ let s= 0;
+ s = 2;
+
+ 
+
+
+
+

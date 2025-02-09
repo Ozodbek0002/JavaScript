6b24a2c3573d39080f2f70532c45921f2390181b
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-02-09 19:30:20 +0500

    10-dars

---
+++ 10-dars/index.js
@@ -0,0 +1,47 @@
+// Number
+// const age = "27"; // string
+// const amount = "123.45"; //     string
+// let name = "Ozodbek"; // string
+
+// console.log( Number(age) ); // 27 // number
+// console.log( parseInt(age) ); // 123 (butun son)
+// console.log( parseFloat(amount) ); // 123.45 (o'nlik son)
+// console.log( Number(name) ) // NaN
+
+
+
+
+// String
+const age = 27;
+
+console.log(  String(age) ); // "27" // string
+console.log(  age.toString() ); // "27" // string
+
+
+
+// Boolean
+console.log(Boolean(0)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean("hello")); // true
+console.log(Boolean("")); // false
+
+
+
+// Type coercion
+
+console.log( 1 + 2  ); // 3
+console.log( 1 + "2" ); // "12" // numberni stringga aylantirish
+
+console.log( '2'*3 ); // 6 // Stringni numberga aylantirish
+
+
+console.log(true + 1); // 2 (true 1 ga aylanadi)
+console.log(false + 5); // 5 (false 0 ga aylanadi)
+
+
+
+
+
+
+
+
---
+++ 9-dars/index.js
@@ -0,0 +1,14 @@
+let age = 12; // number
+let name = "Ozodbek"; // 
+let isMarried = false; // boolean
+let Doc = undefined; // undefined
+let car = null; // null
+let solary = 9007199254740991; // BigInt
+let char = "A"; // Symbol
+
+
+console.log(typeof name) // string
+console.log(typeof age) // number
+console.log(typeof isMarried) // boolean
+console.log(typeof Doc) // undefined
+console.log(typeof car) // null

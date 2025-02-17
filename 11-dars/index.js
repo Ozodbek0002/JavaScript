7c6ab47ed5ae45d9aef75d2698e6d58911cfbb19
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-02-17 14:58:34 +0500

    test

---
+++ 11-dars/index.js
@@ -0,0 +1,28 @@
+// let son = "5" // string
+
+// son = +son // number
+// son = Number(son) // number
+
+// Arifmetik amallar
+
+let son = 5 // number
+
+console.log( 10%2 ); // 0
+console.log( 10%11 ); // 10
+console.log( 10**3 ); // 1000
+console.log( ++son ); // 6
+console.log( son++ ); // 6
+console.log( son ); // 7
+console.log( --son ); // 6
+console.log( son-- ); // 6
+console.log( son ); // 5
+
+son += 5; // son = son + 5; // 10
+son -= 5; // son = son - 5; // 5
+son *= 5; // son = son * 5; // 25
+son /= 5; // son = son / 5; // 5
+
+son%=5; // son = son % 5; // 0
+son=5; 
+son**=2; // son = son ** 2; // 25
+
